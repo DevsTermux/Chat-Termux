e4c88f0885d053a7277fc08dcfbab74db16cae0b
DevsTermux <47542550+DevsTermux@users.noreply.github.com>
2019-02-11 20:06:44 -0300

    Update client.py

--- client.py
+++ client.py
@@ -1,28 +1,24 @@
 import socket
-import select
 import sys
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+cli_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 if len(sys.argv) != 3:
 	print ("Modo correto de usar: script, IP Address, port number")
 	exit()
 IP_Address = str(sys.argv[1])
 Port = int(sys.argv[2])
 user = input("Seu nome: ")
-server.connect((IP_Address, Port))
+cli_sock.connect((IP_Address, Port))
 
 while True:
-	sockets_list = [sys.stdin, server]
-	read_sockets, write_sockets, error_sockets = select.select(sockets_list, [], [])
-	for socks in read_sockets:
-		if socks == server:
-			message = socks.recv(2048)
-			print (message)
-		else:
-			message = sys.stdin.readline()
-			server.send(user + message)
-			sys.stdout.write("<"+user+">")
-			sys.stdout.write(message)
-			sys.stdout.flush()
+	data = input('> ')
+	print ('data= ', data)
+	if not data:
+		break
+	cli_sock.send(data)
+	data = cli_sock.recv(2048)
+	if not data:
+		break
+	print (data)
 
-server.close()
+cli_sock.close()
