9ccc26b11a3977dad73932847f3ce01efd37771d
DevsTermux <47542550+DevsTermux@users.noreply.github.com>
2019-02-11 22:59:44 -0300

    Update server.py

--- server.py
+++ server.py
@@ -5,8 +5,8 @@ import socket
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-ipaddr = str(sys.argv[1])
-port = int(sys.argv[2])
+ipaddr = input("IP: ")
+port = input("PORT: ")
 server.bind((IP_Address, Port))
 server.listen(50)
 
@@ -18,6 +18,6 @@ while True:
 		data = cli_sock.recv(2048)
 		if not data:
 			break
-		#cli_sock.send(('[%s] %s' %(ctime(), data)))
+		cli_sock.send(data)
 	cli_sock.close()
 server.close()
